94b38e83a24d291ae49c87b4b10cd2c2a0834a4a
Krisztian Korosi <koeroesi86@gmail.com>
2022-03-13 16:37:24 +0100

    Added map of locations to filtered needs

--- src/components/map-block/index.tsx
+++ src/components/map-block/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC, ReactNode} from "react";
+import React, {FC, ReactNode, useEffect, useRef} from "react";
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {LatLngExpression} from "leaflet";
 
@@ -9,28 +9,35 @@ interface MapBlockProps {
   markers: { lat: number; lng: number; popup: ReactNode }[];
 }
 
-const MapBlock: FC<MapBlockProps> = ({ markers, className, center, zoom = 6 }) => (
-  <MapContainer center={center} zoom={zoom} className={className}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {markers.map((marker) => (
-      <Marker
-        key={`marker-${marker.lat}-${marker.lng}`}
-        position={{ lat: marker.lat, lng: marker.lng }}
-        ref={m => {
-          if (markers.length === 1 && m && m.openPopup) {
-            setTimeout(() => m.openPopup(), 10);
-          }
-        }}
-      >
-        <Popup>
-          {marker.popup}
-        </Popup>
-      </Marker>
-    ))}
-  </MapContainer>
-);
+const MapBlock: FC<MapBlockProps> = ({ markers, className, center, zoom = 6 }) => {
+  const timer = useRef();
+  useEffect(() => {
+    if (timer.current) clearTimeout(timer.current);
+  }, [markers])
+  return (
+    <MapContainer center={center} zoom={zoom} className={className}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {markers.map((marker) => (
+        <Marker
+          key={`marker-${marker.lat}-${marker.lng}`}
+          position={{lat: marker.lat, lng: marker.lng}}
+          ref={m => {
+            if (markers.length === 1 && m && m.openPopup) {
+              // @ts-ignore
+              timer.current = setTimeout(() => m.openPopup(), 10);
+            }
+          }}
+        >
+          <Popup>
+            {marker.popup}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
 
 export default MapBlock;
--- src/components/needs/index.tsx
+++ src/components/needs/index.tsx
@@ -1,13 +1,26 @@
 import React, {FC, useCallback, useEffect, useState} from "react";
 import debounce from "lodash.debounce";
-import {Need} from "../../types";
-import {Link, List, ListItem, ListItemIcon, ListItemText, TextField} from "@mui/material";
+import {Location, Need} from "../../types";
+import {Link, List, ListItem, ListItemIcon, ListItemText, TextField, Theme} from "@mui/material";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import {Link as RLink} from "react-router-dom";
 import {FormattedMessage, useIntl} from "react-intl";
 import {ApiClient, sortByNames} from "../../utils";
+import MapBlock from "../map-block";
+import {createStyles, makeStyles} from "@mui/styles";
+import {LatLngExpression} from "leaflet";
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  map: {
+    height: '400px',
+    width: '100%',
+    minWidth: '200px',
+    boxSizing: 'border-box',
+  },
+}));
 
 const api = new ApiClient<Need, undefined, { s: string }>('needs');
+const apiLocations = new ApiClient<Location>('locations');
 
 const Needs: FC = () => {
   const intl = useIntl();
@@ -16,6 +29,12 @@ const Needs: FC = () => {
     setTerm(s);
   }, 200), []);
   const [listing, setListing] = useState<Need[]>([]);
+  const [locations, setLocations] = useState<Location[]>([]);
+  const styles = useStyles();
+  const [center] = useState<LatLngExpression>({
+    lat: 47.497913,
+    lng: 19.040236,
+  });
 
   useEffect(() => {
     api.all({ s: term })
@@ -23,6 +42,18 @@ const Needs: FC = () => {
       .then((data) => setListing(data))
       .catch(console.error);
   }, [term]);
+
+  useEffect(() => {
+    if (term) {
+      const locationIds = Array.from(new Set(listing.map((n) => n.locationId)));
+      Promise.all(locationIds.map((id) => apiLocations.one(id)))
+        .then((locations) => locations.filter((l) => l.location))
+        .then(setLocations)
+        .catch(console.error);
+    } else {
+      setLocations([]);
+    }
+  }, [listing, term])
 
   return (
     <>
@@ -33,7 +64,7 @@ const Needs: FC = () => {
         fullWidth
         sx={{ my: 2 }}
       />
-      <List sx={{ maxHeight: 400, overflow: 'auto' }}>
+      <List sx={{ maxHeight: 400, overflow: 'auto', mb: 2 }}>
         {listing.map(need => (
           <ListItem key={`need-${need.id}`}>
             <ListItemIcon>
@@ -46,6 +77,37 @@ const Needs: FC = () => {
           </ListItem>
         ))}
       </List>
+      {locations.length > 0 && (
+        <MapBlock
+          center={center}
+          zoom={6}
+          className={styles.map}
+          markers={locations.map((loc) => ({
+            lat: loc.location.lat,
+            lng: loc.location.lng,
+            popup: (
+              <>
+                <Link
+                  to={`/locations/${loc.id}`}
+                  component={RLink}
+                  color="inherit"
+                  sx={{ display: 'block' }}
+                >
+                  {loc.location.text}
+                </Link>
+                <Link
+                  href={`https://maps.google.com/maps?q=${loc.location.lat},${loc.location.lng}`}
+                  target="_blank"
+                  color="inherit"
+                  sx={{ display: 'block' }}
+                >
+                  Google
+                </Link>
+              </>
+            ),
+          }))}
+        />
+      )}
     </>
   )
 }
