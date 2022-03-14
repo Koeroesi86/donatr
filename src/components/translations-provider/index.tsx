32eeba4b24952ec7c10a4ed8959303def5d88ba4
Krisztian Korosi <koeroesi86@gmail.com>
2022-03-14 21:35:04 +0100

    Minor cleanup

--- src/components/translations-provider/index.tsx
+++ src/components/translations-provider/index.tsx
@@ -1,6 +1,5 @@
 import React, {createContext, FC, useEffect, useState} from "react";
 import {TranslationsResource} from "../../types";
-import {CircularProgress} from "@mui/material";
 import useApiClient from "../../hooks/useApiClient";
 
 export const TranslationsContext = createContext<TranslationsResource[]>([]);
@@ -12,20 +11,14 @@ const TranslationsProvider: FC<TranslationsProviderProps> = ({ children }) => {
   const [translations, setTranslations] = useState<TranslationsResource[]>([]);
   
   useEffect(() => {
-    api.all().then((t) => {
-      setTranslations(t);
-      console.log('translations', t)
-    });
+    api.all().then(setTranslations);
   }, [api]);
-  
-  // if (translations.length === 0) {
-  //   return <CircularProgress />;
-  // }
+
   return (
     <TranslationsContext.Provider value={translations}>
       {children}
     </TranslationsContext.Provider>
-  )
+  );
 }
 
 export default TranslationsProvider;
